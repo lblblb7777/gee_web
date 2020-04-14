ed32cd0569c9d40a9e5c97823e30fb4607c2e9ba
LW <365457604@qq.com>
2020-04-14 15:07:25 +0800

    add recovery

--- main.go
+++ main.go
@@ -18,21 +18,35 @@ func onlyForV2() gee.HandlerFunc {
 	}
 }
 
+//func main() {
+//	r := gee.New()
+//	r.Use(gee.Logger()) // global midlleware
+//	r.GET("/", func(c *gee.Context) {
+//		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+//	})
+//
+//	v2 := r.Group("/v2")
+//	v2.Use(onlyForV2()) // v2 group middleware
+//	{
+//		v2.GET("/hello/:name", func(c *gee.Context) {
+//			// expect /hello/geektutu
+//			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+//		})
+//	}
+//
+//	r.Run(":9999")
+//}
+
 func main() {
 	r := gee.New()
-	r.Use(gee.Logger()) // global midlleware
 	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		c.String(http.StatusOK, "Hello Geektutu\n")
 	})
-
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2()) // v2 group middleware
-	{
-		v2.GET("/hello/:name", func(c *gee.Context) {
-			// expect /hello/geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-		})
-	}
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *gee.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
+	})
 
 	r.Run(":9999")
 }
